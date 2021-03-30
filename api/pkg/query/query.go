428e1fd8faca5829a8c89e9b3123f3d8c799a12d
WaScript3r <wascript3r@gmail.com>
2021-03-30 18:28:03 +0300

    added query name parameter when returning active queries list

--- api/pkg/query/query.go
+++ api/pkg/query/query.go
@@ -1,8 +1,9 @@
 package query
 
 type QueryRes struct {
-	ID  int    `json:"id"`
-	URL string `json:"url"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
 type GetAllRes struct {
