0f3e4ac8ae87994b3a023a60fe36a6c863eb7b30
0_djek <vaiciukyynas1@gmail.com>
2021-05-06 11:03:53 +0300

    Fixed 3rd graph

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 ReactDOM.render(
   <React.StrictMode>
